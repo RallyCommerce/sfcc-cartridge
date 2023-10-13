7d4c567bcd907fa9b9d44f50e348f31577f4f611
zenkkor <rok@rallyon.com>
2023-10-13 16:17:08 +0200

    Stable release

--- plugin_rally/cartridges/plugin_rally/cartridge/client/default/js/rally/rally.js
+++ plugin_rally/cartridges/plugin_rally/cartridge/client/default/js/rally/rally.js
@@ -3,12 +3,12 @@
 module.exports = {
     basketChange: function () {
         $('body').on('cart:update product:afterAddToCart', function (event, data) {
-            if (data.action === 'Cart-AddProduct') {
+            if (data && data.action === 'Cart-AddProduct') {
                 window.RallyCheckoutData.id = data.cart.basketId;
                 if (data.cart.yotpoCartTokken && window.RallyCheckoutData.customerData) {
                     window.RallyCheckoutData.customerData.externalCartToken = encodeURIComponent(data.cart.yotpoCartTokken);
                 }
-            } else if (data.action === 'Cart-RemoveProductLineItem') {
+            } else if (data && data.action === 'Cart-RemoveProductLineItem') {
                 window.RallyCheckoutData.id = data.basket.basketId;
                 if (data.basket.yotpoCartTokken && window.RallyCheckoutData.customerData) {
                     window.RallyCheckoutData.customerData.externalCartToken = encodeURIComponent(data.basket.yotpoCartTokken);
