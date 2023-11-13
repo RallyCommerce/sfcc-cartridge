b978df334c5c0bf000da18cbf4ed2dbc7e26156c
donparapidos <goran.parapid@gmail.com>
2023-11-13 11:56:37 +0000

    v1.0.20 Stable release

--- plugin_rally/cartridges/plugin_rally/cartridge/scripts/hooks/onRequest.js
+++ plugin_rally/cartridges/plugin_rally/cartridge/scripts/hooks/onRequest.js
@@ -13,6 +13,19 @@ exports.onRequest = function () {
             if (!session.privacy.basketId || session.privacy.basketId !== currentBasket.getUUID()) {
                 session.privacy.basketId = currentBasket.getUUID();
             }
+            var Site = require('dw/system/Site');
+            var cookieToOrderSetting = Site.getCurrent().getCustomPreferenceValue('rallyCookieToOrderMapping');
+            if (!empty(cookieToOrderSetting)) {
+                var cookieToOrderMap = JSON.parse(cookieToOrderSetting);
+                var Transaction = require('dw/system/Transaction');
+                cookieToOrderMap.forEach(function (param) {
+                    if (request.httpCookies[param.cookieName] && (!(param.attrName in currentBasket.custom) || currentBasket.custom[param.attrName] !== request.httpCookies[param.cookieName].value)) {
+                        Transaction.wrap(function () {
+                            currentBasket.custom[param.attrName] = request.httpCookies[param.cookieName].value;
+                        });
+                    }
+                });
+            }
         }
     }
 
